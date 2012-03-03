15f4db4c4371cced894ebb1aa9ad2cfae620f45a
Josh Wachsman <jwachs2412@gmail.com>
2012-03-02 20:33:15 -0500

    nice

---
+++ addProduct.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Add Products</title>
+
+</head>
+
+<body>
+
+<form action="scsave.php" method="post">
+<div>Product Name:<input type="text" name="pname" id="pname" value=""></div>
+<div>Price:<input type="text" name="pprice" id="pprice" value=""></div>
+<div><input type="submit" name="submit" value="Submit"></div>
+
+
+</form>
+
+</body>
+</html>
